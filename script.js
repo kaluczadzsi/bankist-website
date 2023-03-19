cac26514e706aa8264985d76e15644d9742d757f
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-03-19 12:16:00 +0100

    Styles, attributes, classes change

--- script.js
+++ script.js
@@ -68,3 +68,47 @@ document.querySelector('.btn--close-cookie').addEventListener('click', () => {
   // message.remove();
   message.parentElement.removeChild(message);
 });
+
+// Styles
+
+message.style.backgroundColor = '#37383d';
+message.style.width = '120%';
+
+console.log(message.style.color);
+console.log(message.style.backgroundColor);
+console.log(getComputedStyle(message).height);
+
+message.style.height = Number.parseFloat(
+  (getComputedStyle(message).height, 10) + 30 + 'px'
+);
+
+// document.documentElement.style.setProperty('--color-primary', 'orangered');
+
+// Attributes
+const logo = document.querySelector('.nav__logo');
+console.log(logo.src);
+console.log(logo.alt);
+console.log(logo.className);
+console.log(logo.getAttribute('designer'));
+
+logo.alt = 'Beautiful minimalist logo';
+logo.setAttribute('company', 'bankist');
+
+console.log(logo.src); // Absolute
+console.log(logo.getAttribute('src')); // Relative
+
+const link = document.querySelector('.nav__link--btn');
+console.log(link.getAttribute('href'));
+console.log(link.href);
+
+// Data attributes
+console.log(logo.dataset.versionNumber);
+
+// Classes
+logo.classList.add('c');
+logo.classList.remove('c', 'j');
+logo.classList.toggle('c');
+logo.classList.contains('c');
+
+// Don't use
+// logo.className = 'gabor';
