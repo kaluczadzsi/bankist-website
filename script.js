cdea80b7e313125b122d44abbf3c75475458802a
Kalucza Gábor <kaluczadzsi@gmail.com>
2023-03-21 17:26:50 +0100

    Handling events

--- script.js
+++ script.js
@@ -141,3 +141,19 @@ btnScrollTo.addEventListener('click', e => {
     behavior: 'smooth',
   });
 });
+
+const h1 = document.querySelector('h1');
+
+const alertH1 = () => {
+  alert('addEventListener: Great! You are reading the heading :D');
+};
+
+h1.addEventListener('mouseenter', alertH1);
+
+setTimeout(() => {
+  h1.removeEventListener('mouseenter', alertH1);
+}, 3000);
+
+// h1.onmouseenter = () => {
+//   alert('addEventListener: Great! You are reading the heading :D');
+// };
